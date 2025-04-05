5088cbda5c52be474062b96ed998821ce16db9b9
Andres Garrido <aag22j@gmail.com>
2025-04-05 02:48:57 -0400

    Third Lecture changes made.

---
+++ Library.eCommerce/Models/Product.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Dynamic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Spring2025_andresg.Models
+{
+    public class Product
+    {
+
+        public int Id { get; set; } // auto property
+
+        public string? Name { get; set; } // auto property
+
+        public string? Display
+        {
+            get
+            {
+                return $"{Id}. {Name}";
+            }
+        }
+        public Product()
+        {
+            Name = string.Empty;
+        }
+
+        public override string ToString()
+        {
+            return Display ?? string.Empty;
+        }
+
+    }
+}
